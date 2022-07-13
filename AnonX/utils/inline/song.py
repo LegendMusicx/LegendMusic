83ce608e5e99d1900fcddc866afe349107e39ee9
LegendMusicx <109218848+LegendMusicx@users.noreply.github.com>
2022-07-13 20:11:24 +0530

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="•❤️ sᴜᴩᴩᴏʀᴛ ❤️•", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
