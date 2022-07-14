a63b0670183cc1ea9f9ad82d892a96485032f876
LegendMusicx <109218848+LegendMusicx@users.noreply.github.com>
2022-07-15 05:09:58 +0530

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -12,7 +12,7 @@ class LegendMusic_Robot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "LegendMusic_Robotc",
+            "AnonXMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
